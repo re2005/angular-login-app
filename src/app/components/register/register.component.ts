32f8fc0c365613c3e1c095d7f9314a1c2b61523f
Renato Cardoso <re2005@gmail.com>
2019-11-17 15:09:50 +0100

    refactor: adjust style make redirection to login

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {AuthService} from '../../services/auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-register',
@@ -9,13 +10,16 @@ import {AuthService} from '../../services/auth/auth.service';
 })
 export class RegisterComponent implements OnInit {
 
+    loading = false;
+
     registrationForm: FormGroup = new FormGroup({
         name: new FormControl('', [Validators.required]),
         username: new FormControl('', [Validators.required]),
         password: new FormControl('', [Validators.required])
     });
 
-    constructor(private authService: AuthService) {
+    constructor(private authService: AuthService,
+                public router: Router) {
     }
 
     ngOnInit() {
@@ -24,8 +28,14 @@ export class RegisterComponent implements OnInit {
 
     onSubmit() {
         if (this.registrationForm.valid) {
-            this.authService.setUser(this.registrationForm.value);
+            this.loading = true;
+            setTimeout(() => {
+                this.authService.setUser(this.registrationForm.value);
+                this.router.navigate(['login']);
+                this.loading = false;
+            }, 1500);
         }
+
     }
 
 }
